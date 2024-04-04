8417930d09b853f9d1d1d304032390ad19de40df
CodeWithYash99 <yashwanthrangu99@gmail.com>
2024-04-04 18:23:43 +0530

    Updates

---
+++ static/js/952.55ae5ec1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkregister_login_form=self.webpackChunkregister_login_form||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.55ae5ec1.chunk.js.map
